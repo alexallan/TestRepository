f8c155bd434ac715749d751daada68bc2a87b11a
Alex Allan <alex.j.m.allan@gmail.com>
2013-05-10 17:06:39 +0100

    INFO: first attempt = abject failure. Cant seem to get the javascript to load from the .js file

---
+++ testJS/WebContent/Timer.js
@@ -0,0 +1,40 @@
+//var Timer;
+//var TotalSeconds;
+//
+//
+//function CreateTimer(TimerID, Time)
+//{
+//	Timer= document.getElementById(TimerID);
+//	TotalSeconds = Time;
+//	
+//}
+
+function myFunction()
+{
+alert("Hello World!");
+}
+function helloWorld()
+{
+
+	alert("PIPE!");
+//	Timer.innerHTML = 'Go fuck yourself';
+//	document.getElementById("nonce").innerHTML = "go fuck yourself";
+}
+
+//UpdateTimer()
+//{
+//	window.setTimeout("Tick()", 1000);
+//}
+
+//function Tick()
+//{
+//	TotalSeconds -= 1;
+//	UpdateTimer();
+//	window.setTimeout("Tick()", 1000);
+//}
+//
+//function UpdateTimer()
+//
+//{
+//	Timer.innerHTML = TotalSeconds;
+//}
